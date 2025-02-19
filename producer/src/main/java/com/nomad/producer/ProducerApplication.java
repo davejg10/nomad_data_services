2e61965222a5035bbee9109d2fd7f3078f8712ae
David <davejohngoddard@gmail.com>
2025-02-19 14:13:11 +0000

    Deploying function app to cloud

--- producer/src/main/java/com/nomad/producer/ProducerApplication.java
+++ producer/src/main/java/com/nomad/producer/ProducerApplication.java
@@ -2,13 +2,13 @@ package com.nomad.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class ProducerApplication {
-//
-//	public static void main(String[] args) {
-//
-//		SpringApplication.run(ProducerApplication.class, args);
-//	}
-//
-//}
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+}
---
+++ producer/src/main/java/com/nomad/producer/ProducerHandler.java
@@ -0,0 +1,38 @@
+package com.nomad.producer;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+@Slf4j
+@Component
+public class ProducerHandler {
+
+    public static String EXECUTION_CONTEXT = "executionContext";
+
+    private static AtomicInteger count = new AtomicInteger();
+
+    @Autowired
+    private Consumer<Message<String>> processAndShutdown;
+
+    @FunctionName("processAndShutdown")
+    public void execute(@TimerTrigger(name = "keepAliveTrigger", schedule = "* */5 * * * *") String timerInfo,
+                        ExecutionContext context) {
+
+        Message<String> message = MessageBuilder
+                .withPayload(timerInfo)
+                .setHeader(EXECUTION_CONTEXT, context)
+                .build();
+
+        this.processAndShutdown.accept(message);
+    }
+
+}
