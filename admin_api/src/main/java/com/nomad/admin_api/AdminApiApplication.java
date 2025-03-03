9b39093c40748fe0340b0fbb2a1ce1b55a7adca9
David <davejohngoddard@gmail.com>
2025-03-03 21:27:22 +0000

    using managed identity client:

--- admin_api/src/main/java/com/nomad/admin_api/AdminApiApplication.java
+++ admin_api/src/main/java/com/nomad/admin_api/AdminApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.nomad.library.domain"}) // Required for the Neo4j Entities
+@EntityScan(basePackages = {"com.nomad.library.domain", "com.nomad.admin_api.domain" }) // Required for the Neo4j Entities
 public class AdminApiApplication {
     
     public static void main(String[] args) {
--- admin_api/src/main/java/com/nomad/admin_api/SqlCountryRepository.java
+++ admin_api/src/main/java/com/nomad/admin_api/SqlCountryRepository.java
@@ -1,11 +1,13 @@
 package com.nomad.admin_api;
 
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nomad.admin_api.domain.SqlCountry;
 
 @Repository
-public interface SqlCountryRepository extends CrudRepository<SqlCountry, String> {
+public interface SqlCountryRepository extends CrudRepository<SqlCountry, UUID> {
 
 } 
