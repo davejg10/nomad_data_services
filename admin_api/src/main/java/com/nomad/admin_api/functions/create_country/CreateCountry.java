41f3ef9e6cea54d64f54f7c5f166b174c5551540
David <davejohngoddard@gmail.com>
2025-03-03 21:02:06 +0000

    using managed identity client:

--- admin_api/src/main/java/com/nomad/admin_api/functions/create_country/CreateCountry.java
+++ admin_api/src/main/java/com/nomad/admin_api/functions/create_country/CreateCountry.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
