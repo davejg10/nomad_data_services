1d3c8e85dd752328e43c541a7a7b9778f9cf8e0f
David <davejohngoddard@gmail.com>
2025-03-06 21:17:03 +0000

    Added app insights to one2goscraper

--- one2goasia/src/main/java/com/nomad/one2goasia/One2GoAsiaApplication.java
+++ one2goasia/src/main/java/com/nomad/one2goasia/One2GoAsiaApplication.java
@@ -1,15 +1,32 @@
 package com.nomad.one2goasia;
 
+import java.util.Objects;
+import org.springframework.core.env.Environment;
+
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@Log4j2
 @SpringBootApplication
 @ComponentScan({"com.nomad.library.config", "com.nomad.one2goasia"})
 public class One2GoAsiaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(One2GoAsiaApplication.class, args);
+		Environment environment = SpringApplication.run(One2GoAsiaApplication.class, args).getEnvironment();
+
+		String profile = environment.getProperty("spring.profiles.active", "local");
+
+		if (!Objects.equals(profile, "local")) {
+			log.info("Attaching application insights");
+			// Note; the application insights connection string is set as `APPLICATIONINSIGHTS_CONNECTION_STRING` environment variable.
+			ApplicationInsights.attach();
+		}
+
 	}
 
 }
