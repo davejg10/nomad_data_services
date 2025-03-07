183a44482a76a2055942eda254b674e47b0a2a5a
David <davejohngoddard@gmail.com>
2025-03-07 15:32:49 +0000

    Passing values

--- job_orchestrator/src/main/java/com/nomad/job_orchestrator/JobOrchestratorApplication.java
+++ job_orchestrator/src/main/java/com/nomad/job_orchestrator/JobOrchestratorApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.microsoft.applicationinsights.attach.ApplicationInsights;
 
