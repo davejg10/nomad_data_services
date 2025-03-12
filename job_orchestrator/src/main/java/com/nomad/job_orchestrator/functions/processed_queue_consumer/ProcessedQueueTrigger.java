7858e41bf902ca39a4867014cc53e67664560476
David <davejohngoddard@gmail.com>
2025-03-12 23:10:42 +0000

    Trying to do in cloud

--- job_orchestrator/src/main/java/com/nomad/job_orchestrator/functions/processed_queue_consumer/ProcessedQueueTrigger.java
+++ job_orchestrator/src/main/java/com/nomad/job_orchestrator/functions/processed_queue_consumer/ProcessedQueueTrigger.java
@@ -1,22 +1,14 @@
 package com.nomad.job_orchestrator.functions.processed_queue_consumer;
 
-
-import java.util.Map;
-import java.util.logging.Level;
-
 import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
-import com.nomad.scraping_library.domain.ScraperRequest;
 import com.nomad.scraping_library.domain.ScraperResponse;
 
 import lombok.extern.log4j.Log4j2;
