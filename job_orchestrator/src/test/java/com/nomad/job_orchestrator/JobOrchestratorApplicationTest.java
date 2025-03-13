6e67b99d43f32f36f869fa02978ab538a9327b68
David <davejohngoddard@gmail.com>
2025-03-13 13:25:37 +0000

    Trying to use my custom appisngiths jar

--- job_orchestrator/src/test/java/com/nomad/job_orchestrator/JobOrchestratorApplicationTest.java
+++ job_orchestrator/src/test/java/com/nomad/job_orchestrator/JobOrchestratorApplicationTest.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
-import com.microsoft.applicationinsights.TelemetryClient;
 import com.nomad.scraping_library.connectors.ServiceBusBatchSender;
 import com.nomad.scraping_library.domain.ScraperRequest;
 
@@ -17,9 +16,6 @@ public class JobOrchestratorApplicationTest {
     
     @MockitoBean
     private ServiceBusSenderClient serviceBusSenderClient;
-    
-    @MockitoBean
-    private TelemetryClient telemetryClient;
 
     @Test
 	void  contextLoads() {
